fc6f9567434cf3bd4e9f9c304a1f1d4ad707f730
Alin Babu <alinbabu2010@gmail.com>
2020-09-15 21:48:15 +0530

    Personalized message script added

--- sample-sites/scripts/myscripts.js
+++ sample-sites/scripts/myscripts.js
@@ -1,4 +1,6 @@
-/* global document */
+/* global document,window,localStorage */
+
+/* Code for image changer */
 var dogImage = document.querySelector('img');
 
 dogImage.onclick = function () {
@@ -9,4 +11,26 @@ dogImage.onclick = function () {
     } else {
         dogImage.setAttribute('src', 'images/buliding.jpg');
     }
+};
+
+/* Personalized welcome message code */
+
+var nameButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+    'use strict';
+    var myName = window.prompt("Please enter your name.");
+    localStorage.setItem('name', myName);
+    myHeading.textContent = "Have a nice day, " + myName;
+}
+if (!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    var storedName = localStorage.getItem('name');
+    myHeading.textContent = "Have a nice day" + storedName;
+}
+nameButton.onclick = function () {
+    'use strict';
+    setUserName();
 };
