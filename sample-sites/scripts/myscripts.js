521592dbb403568fadc72da3d0e64754f93c4a72
Alin Babu <alinbabu2010@gmail.com>
2020-09-15 21:31:04 +0530

    Added image change script.

--- sample-sites/scripts/myscripts.js
+++ sample-sites/scripts/myscripts.js
@@ -1,3 +1,12 @@
 /* global document */
-var myHeading = document.querySelector('h1');
-myHeading.textContent = "Be kind";
+var dogImage = document.querySelector('img');
+
+dogImage.onclick = function () {
+    'use strict';
+    var myImages = dogImage.getAttribute('src');
+    if (myImages === 'images/buliding.jpg') {
+        dogImage.setAttribute('src', 'images/nature.jpg');
+    } else {
+        dogImage.setAttribute('src', 'images/buliding.jpg');
+    }
+};
