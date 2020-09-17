4ff3633718ae1a0ab6b8a52fa6b4ef75a283e315
Alin Babu <alinbabu2010@gmail.com>
2020-09-17 21:13:59 +0530

    Countdown Clock App

--- Countdown Clock/scripts.js
+++ Countdown Clock/scripts.js
@@ -13,29 +13,24 @@ function getTimeRemaining(endtime) {
         'seconds': seconds
     };
 }
-
 // Function to intialize the clock 
-function intializeClock(id, endtime) {     
+function intializeClock(id, endtime) {
     var clock = document.getElementById(id);
     var daysSpan = clock.querySelector('.days');
     var hoursSpan = clock.querySelector('.hours');
     var minutesSpan = clock.querySelector('.minutes');
     var secondsSpan = clock.querySelector('.seconds');
-
     //Function to update the clock
     function updateClock() {
         var t = getTimeRemaining(endtime);
-
         daysSpan.innerHTML = t.days;
         hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
         minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
         secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-
         if (t.total <= 0) {
             clearInterval(timeinterval);
         }
     }
-
     updateClock();
     var timeinterval = setInterval(updateClock, 1000);
 }
