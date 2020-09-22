5e8085d5d44f8d0b4cff84e4dc618826395bf657
Alin Babu <alinbabu2010@gmail.com>
2020-09-22 19:18:22 +0530

    Python list examples

---
+++ Python/mylist.py
@@ -0,0 +1,12 @@
+animals = ["bear", "tiger", "lion", "panda", "elephant"]
+'''for x in animals:
+    print(x)
+print(animals)'''
+print(animals[0])
+print(animals[-1])  # Print last item
+print(animals[1:])  # Print from index 1
+print(animals[1:3])  # Print index 1 and 2
+
+# Modify item in list
+animals[0] = "dog"
+print(animals)
