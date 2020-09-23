ed9b2403c61e8baf38d3209cc2e32db28dcf178b
Alin Babu <alinbabu2010@gmail.com>
2020-09-23 21:15:15 +0530

    Encapsulation in Python

---
+++ Python/encap.py
@@ -0,0 +1,35 @@
+# Encapsulation in Python #
+
+
+class Cars:
+
+    def __init__(self, speed, color):
+        self.__speed = speed
+        self.__color = color
+
+    def setSpeed(self, value):
+        self.__speed = value
+
+    def getSpeed(self):
+        return self.__speed
+
+    def setColor(self, value):
+        self.__color = value
+
+    def getColor(self):
+        return self.__color
+
+
+ford = Cars(250, "Green")
+nissan = Cars(300, "Red")
+toyota = Cars(350, "Blue")
+
+ford.setSpeed(450)
+ford.setColor("Brown")
+
+# Below teo lines have no effect because speed and color are private.
+ford.speed = 500
+ford.color = "Pink"
+
+print(ford.getSpeed())
+print(ford.getColor())
