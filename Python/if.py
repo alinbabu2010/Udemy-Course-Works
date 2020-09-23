0828935338aa91e8cecec7944df4af1343bb7e55
Alin Babu <alinbabu2010@gmail.com>
2020-09-23 12:17:06 +0530

    Python control statement code snippets

---
+++ Python/if.py
@@ -0,0 +1,22 @@
+# If condition
+a = 7
+b = 8
+if a < b:
+    print(a, "is smaller than", b)
+
+
+# If Else condition
+a = 5
+b = 4
+if a < b:
+    print(a, "is smaller than", b)
+else:
+    print(b, "is smaller than", a)
+
+# Elif statement
+a = 7
+b = 8
+if a > b:
+    print(a, "is larger than", b)
+elif b >= a:
+    print(b, "is larger than", a)
