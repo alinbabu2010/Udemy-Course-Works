c8a72fa3f8b0379246625e6a4feabb2a6795011c
Alin Babu <alinbabu2010@gmail.com>
2020-09-23 12:29:40 +0530

    Python loop statement code snippets

---
+++ Python/loop.py
@@ -0,0 +1,19 @@
+# While loop statement
+i = 1
+while i < 7:
+    print(i)
+    i += 1
+
+print("\n")
+
+# For loop statement
+fruits = ["Grapes", "Berries", "Oranges"]
+for fruit in fruits:
+    print(fruit)
+
+# Nested for loop statement
+colors = ["Green", "Yellow", "Purple"]
+fruits = ["Apples", "Banana", "Berries"]
+for color in colors:
+    for fruit in fruits:
+        print(color, fruit)
