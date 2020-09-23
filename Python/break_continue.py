6ce80ccce9561c2a74c806adadc6df3c86fca79f
Alin Babu <alinbabu2010@gmail.com>
2020-09-23 12:39:18 +0530

    Python brak and continue example

---
+++ Python/break_continue.py
@@ -0,0 +1,17 @@
+# Break statement
+i = 1
+while i < 8:
+    print(i)
+    if i == 3:
+        break
+    i += 1
+
+print("\n")
+
+# Contine statement
+i = 0
+while i < 8:
+    i += 1
+    if i == 4:
+        continue
+    print(i)
